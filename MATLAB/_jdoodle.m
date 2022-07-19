7bea65953db0e08d27e221efead87df421123dae
Kartik Padave <kartikpadave.developer@gmail.com>
2022-07-19 11:36:39 +0530

    Add files via upload

---
+++ MATLAB/_jdoodle.m
@@ -0,0 +1,43 @@
+% Question1
+% x = 2^5;
+% y = 2^5-1;
+% z = x/y
+% (1-(1/2^5))-1
+
+% Question2
+% (3*(sqrt(5)-1)/(sqrt(5)+1)*2)-1
+
+% Question3
+% r = pi^(1/3)-1;
+% A = pi*r^2
+
+% Question4
+% exp(3)
+% log(exp(3))
+% log10(exp(3))
+% log10(10^5)
+
+% Question5
+% exp(pi*sqrt(163))
+% sin(2*pi/6) + cos(2*pi/6)
+
+% Question6
+% x = 32*pi
+% y = cosh(2*x)-sinh(2*x)
+
+
+% Question7
+% x = log(17) / log(3)
+
+% Question8
+% i = sqrt(-1)
+% 1 + 3*i/(1 - 3*i)
+
+% Question9
+% i = sqrt(-1)
+% exp(pi/2*i)
+% exp(pi/2i)
+
+% Question10
+cot(0)
+1/tan(inf)
