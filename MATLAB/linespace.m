8291e4dafa6e341b36c9d392705aab71a24437fe
Kartik Padave <kartikpadave.developer@gmail.com>
2022-07-19 12:12:11 +0530

    Add files via upload

---
+++ MATLAB/linespace.m
@@ -0,0 +1,10 @@
+theta = linspace(0, 2*pi, 100);
+x = cos(theta);
+y = sin(theta);
+
+plot(x, y);
+xlabel('x');
+ylabel('ý');
+axis('square');
+axis('equal');
+title('circle');
---
+++ MATLAB/sinewave.m
@@ -0,0 +1,8 @@
+a = 7;
+f = 0.5;
+t = 0:0.1:10;
+y = a*sin(2*pi*f*t);
+plot(t, y);
+xlabel('time');
+ylabel('sine wave');
+title('Plot created by ShinigamiLeo');
