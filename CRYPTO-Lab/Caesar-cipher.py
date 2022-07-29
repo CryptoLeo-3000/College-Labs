4bc14b754e9d883bd03b7d09828439e8b14d152b
CryptoLeo-3000 <padavekartik@gmail.com>
2022-07-29 11:43:38 +0530

    Caesar Cipher

--- CRYPTO-Lab/Caesar-cipher.py
+++ CRYPTO-Lab/Caesar-cipher.py
@@ -1,2 +1,14 @@
-text = input("Enter plain text: ")
+from rsa import encrypt
+
+
+text = input("Enter plain text: ").upper()
 shift = int(input("Enter shift value: "))
+encrypt_text = ""
+
+for i in range(len(text)):
+    char = text[i]
+    encrypt_text += chr((ord(char) + shift-65) % 26 + 65)
+
+print(f"Plain Text: {text}")
+print(f"Shift value: {shift}")
+print(f"Encrypted Text: {encrypt_text}")
