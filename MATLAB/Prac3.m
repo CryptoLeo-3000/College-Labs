0f7197b7a7182e3e3fef09a1cfe857c84ea4ff74
Kartik Padave <kartikpadave.developer@gmail.com>
2022-08-02 12:31:40 +0530

    Practical 3

---
+++ MATLAB/Prac3.m
@@ -0,0 +1,117 @@
+% 4x3 matrix
+% mat = [1:3; 4:6; 7:9; 10:12];
+% disp(mat);
+
+% rot180 not available in MATLAB
+
+% Rotate by 90
+% disp(rot90(mat));
+
+% Flip matrix left to right
+% disp(fliplr(mat));
+
+% Flip matrix up to down
+% disp(flipud(mat));
+
+% Create empty vector
+% vec = [];
+% disp(size(vec));
+% disp(length(vec));
+
+% Vector with 1 element
+% vec = [4];
+% disp(vec);
+% disp(length(vec));
+
+% Input/Output command:
+    % disp - display contents of an array or string.
+    % fscanf - Read formatted data from a file.
+    % format - Control screen display format.
+    % fprintf - Peforms formatted write to screen/file.
+    % input - Display prompt and wait for input.
+
+% Numeric display format:
+    % format short - Displays decimal digit upto 4.
+    % format long - Displays decimal digit upto 14.
+    % format short e - 5 digits plus exponent.
+    % format long e - 14 digits plus exponent.
+    % format bank -  2 decimal digits.
+    % format + - +ve or 0.
+    % format rat - Rational approx.
+    % format compact - Suppress some line feeds.
+    % format loose - Reset to less compact display mode.
+
+% Commands for managing session:
+    % clc - clear command window.
+    % clear - clear workspace memory.
+    % exist - checks existence of file/variable.
+    % global - declares variables to be global.
+    % help - searches for help topic.
+    % quit - stops matlab.
+    % who - Lists current variables.
+    % whos - Lists current variables in long display.
+
+% Special variables and constants:
+    % ans - most recent answer.
+    % eps - accuracy of floating point precision.
+    % i, j - complex numbers/imaginary unit (sqrt(-1)).
+    % Inf - infinity.
+    % NaN - Not a Number;
+    % pi - 3.14....
+   
+% Operators and special characters:
+    % + - addition
+    % - - subtraction
+    % * - multiply
+    % .* - array multiplication
+    % ^ - exponential power
+    % .^ - array exponential
+    % \ - left division
+    % / - right division
+    % .\ - array left division
+    % ./ - array right division
+    % : - creates regular spaced array
+    % () - parentheses
+    % [] - array enclosure
+    % . - decimal point
+    % ... - ellipsis
+    % , - separates values
+    % ; - suppress display format
+    % = - assignment operator
+    % _ - quote
+    % ._ - non conjugate transpose operator
+    
+% System & File commands:
+    % delete - delete a file.
+    % date - display date.
+    % diary - document all file records.
+    % load - load workspace variables from file.
+    % path - display search path.
+    % pwd - display current directory.
+    % save - save workspace variables in file.
+    % what - lists MATLAB files in directory.
+    % wklread - reads .wkl spreadsheet files.
+
+% Matrix command for solving linear eq.:
+    % det - determinant.
+    % inv - inverse.
+    % rank - rank of matrix.
+    % rref - computes reduced row echelon form.
+    % pinv - computes psuedoinverse of matrix.
+    
+% Symbol:
+    % . - plot line dot sign
+    % o - plot line small o sign
+    % + - plot line plus sign
+    % * - plot line star sign
+    % d - plot line diamond sign
+    % v - plot line triangle down sign
+    % ^ - plot line triangle up sign
+    % < - plot line triangle left sign
+    % > - plot line triangle right sign
+    % p - plot line pentagram sign
+    % h - plot line hexagram sign
+    % - - plot line solid
+    % : - plot line dotted
+    % -. - plot line dash dot
+    % -- - plot line dashed
