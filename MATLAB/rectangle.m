eb9240ebdb3569f24ac6bf3c1483808fd70a51f2
Kartik Padave <kartikpadave.developer@gmail.com>
2022-09-13 12:00:25 +0530

    Add files via upload

---
+++ MATLAB/rectangle.m
@@ -0,0 +1,9 @@
+% Calc/ Area and perimeter of rectangle
+l = 10; %Length of rectangle
+b = 2; %Breadth of rectangle
+
+A = l*b; %Area of rectangle
+disp(A);
+
+P = 2*(l+b); %Perimeter of rectangle
+disp(P);
