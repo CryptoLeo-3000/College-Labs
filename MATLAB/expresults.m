29dccdf3ae09e6466226cc9d07e21413ce4b2a47
Kartik Padave <kartikpadave.developer@gmail.com>
2022-09-20 11:35:43 +0530

    Add files via upload

---
+++ MATLAB/expresults.m
@@ -0,0 +1,3 @@
+mat = [4 53.4; 3 44.3; 2 50.0; 1 55.5];
+correctorder = flipud(mat)
+flip_lr = fliplr(mat)
---
+++ MATLAB/plotonepoint.m
@@ -0,0 +1,9 @@
+x = 11;
+y = 48;
+plot(x, y, 'r*')
+
+axis([9 12 35 55])
+xlabel('Time')
+ylabel('Temperature')
+
+title('Time and Temp')
